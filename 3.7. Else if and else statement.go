18ef9a37e1652d3495944cca1084775ad042b123
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-23 20:18:53 +0530

    Update 3.7. Else if and else statement.go

--- 3.7. Else if and else statement.go
+++ 3.7. Else if and else statement.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	x := "james"
 	if 42 > 54 {
-		fmt.Println("42 is less")
-	} else if x == "James" {
+		fmt.Println("42 is less")	
+	} else if x == "James" {//Works if it is just after the parentheses
 		fmt.Println("I am James")
 
 	} else {
