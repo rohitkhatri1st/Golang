06323d054dd78eaed6bae45a8c6f01979d9066de
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-25 20:20:01 +0530

    Update 4.1. Composite literal array.go

--- 4.1. Composite literal array.go
+++ 4.1. Composite literal array.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	arr := [5]int{42, 43, 44, 45, 46}
+	arr := [5]int{42, 43, 44, 45, 46}	//int {42, 43, 44, 45, 46} is a composite literal and [5] defines length of array
 	for i, v := range arr {
 		fmt.Println(i, v)
 	}
