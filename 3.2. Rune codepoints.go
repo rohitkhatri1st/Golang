a356d51ee78eee2c350764204f816bbd4981d7d9
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-25 20:23:33 +0530

    Update 3.2. Rune codepoints.go

--- 3.2. Rune codepoints.go
+++ 3.2. Rune codepoints.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 )
-
+//rune codepoint is an alias for int32
 func main() {
 	for i := 1; i < 27; i++ {
-		fmt.Printf("%d\n\t%#U\n\t%#U\n\t%#U\n", i, i+64, i+64, i+64)
+		fmt.Printf("%d\n\t%#U\n\t%#U\n\t%#U\n", i, i+64, i+64, i+64)	//%#U prints rune code value of passed value.
 	}
 }
