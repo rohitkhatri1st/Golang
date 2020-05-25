0acb7c3bd72a38107eab50089e8a885bce4b01c4
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-25 20:18:48 +0530

    Update 4.2. Composite literal slice.go

--- 4.2. Composite literal slice.go
+++ 4.2. Composite literal slice.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	s := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	s := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}	// int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51} is a compsite literal
 	for i, v := range s {
 		fmt.Println(i, v)
 
