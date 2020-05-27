76dc6aa19b2174cd15d758edf1fc20ac3b7925af
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-27 18:55:38 +0530

    Update 1.4. Creating own type.go

--- 1.4. Creating own type.go
+++ 1.4. Creating own type.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
-type mynew int
+type mynew int	//type is used to define a new type. This statement says mynew has an underlying type "int".
 
-var x mynew
+var x mynew	//x is of type mynew and mynew has an underlying type "int".
 
 func main() {
 	fmt.Println(x)
