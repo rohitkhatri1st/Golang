072e3e9a6763862ebfaa9a106a5396c59276ec41
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-27 18:53:32 +0530

    Update 1.3. Using Sprintf.go

--- 1.3. Using Sprintf.go
+++ 1.3. Using Sprintf.go
@@ -9,7 +9,7 @@ var y = "James Bond"
 var z = true
 
 func main() {
-	a := fmt.Sprintf("%v %v %v", x, y, z)
+	a := fmt.Sprintf("%v %v %v", x, y, z)	//Sprintf formats according to a format specifier and RETURNS the resulting STRING.
 	//%v denotes default format of the value
 	fmt.Println(a)
 }
