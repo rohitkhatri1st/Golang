13ff51577157cc0334d0aad940bc18a8cd0fc030
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-29 22:40:36 +0530

    Update 2.2. Operators.go

--- 2.2. Operators.go
+++ 2.2. Operators.go
@@ -1,10 +1,10 @@
 package main
 
 func main() {
-	a := (42 == 42)
+	a := (42 == 42)	//'==' , '<=' , '>=' , '!=' , '<' , '>' are few ofthe operators
 	b := (42 <= 28)
 	c := (42 >= 42)
-	d := (42 != 42)
+	d := (42 != 42)	//!= is "not equal to" operator, it checks if 42 is not equal to 42
 	e := (42 < 63)
 	f := (42 > 63)
 	println(a, b, c, d, e, f)
