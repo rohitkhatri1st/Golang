94ef5f29942223d5814dca255cf16e4e99ee5ece
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-29 22:37:34 +0530

    Update 2.1. Decimal to binary and hex.go

--- 2.1. Decimal to binary and hex.go
+++ 2.1. Decimal to binary and hex.go
@@ -6,5 +6,5 @@ import (
 
 func main() {
 	a := 42
-	fmt.Printf("%d\n%b\n%#x", a, a, a)
-}
+	fmt.Printf("%d\n%b\n%#x", a, a, a)	//%b and %#x are the format specifiers of binary and hex value starting with 0x respectively.
+}	//%x is the format specifier for hexadecimal value without 0x.
