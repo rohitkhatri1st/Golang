7001c15318170a09620e4fb1c6355f9a8dd32740
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-30 22:40:13 +0530

    Update 2.4. Bit Shifting.go

--- 2.4. Bit Shifting.go
+++ 2.4. Bit Shifting.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 )
-
+//bit shifting takes binary input and then shifts one bit.
 func main() {
-	a := 42
+	a := 42	// 42=101010
 	fmt.Printf("%d\t%b\t%#x\n", a, a, a)
-	b := a << 1
-	fmt.Printf("%d\t%b\t%#x", b, b, b)
+	b := a << 1	//Shifting to left 1 bit of a.
+	fmt.Printf("%d\t%b\t%#x", b, b, b)	//b=1010100=84
 
 }
