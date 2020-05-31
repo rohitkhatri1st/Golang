01758294c816c24d892b4102bea5462b7c084b3c
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-05-31 22:54:32 +0530

    Add files via upload

---
+++ 8. Hands-on exercise #8.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+		"no_dr":           []string{"Being Evil", "Icecream", "Sunsets"},
+	}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+}
