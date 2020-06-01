b1b5c9b28bcd51007b10689a2900838dd8256a53
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-01 22:21:07 +0530

    Update 2.5. Raw String Literal.go

--- 2.5. Raw String Literal.go
+++ 2.5. Raw String Literal.go
@@ -6,6 +6,6 @@ import (
 
 func main() {
 	a := `My name is"Rohit"..
-Who Are YOu?` // `` is raw string literal
+Who Are YOu?` // `` is raw string literal. String inside it is printed AS IT IS.
 	fmt.Println(a)
 }
