8a345344a1f24a0045d72b4f2641b4fcc12e5db0
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-01 22:15:31 +0530

    Add files via upload

---
+++ 10. Hands-on exercise #10.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+		"no_dr":           []string{"Being Evil", "Icecream", "Sunsets"},
+	}
+	m["Mcleod_Todd"] = []string{"Coding", "Teaching", "Movies"}
+	for k, v := range m {
+		fmt.Println("The record of ", k, ":")
+		for i, j := range v {
+			fmt.Println("\t", i, j)
+		}
+	}
+	delete(m, "Mcleod_Todd")
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+}
