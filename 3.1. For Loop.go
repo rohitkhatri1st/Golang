0e6d8899a40c0ab78d0489012655132a4721a8c8
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-02 22:49:17 +0530

    Update 3.1. For Loop.go

--- 3.1. For Loop.go
+++ 3.1. For Loop.go
@@ -5,7 +5,10 @@ import (
 )
 
 func main() {
-	for i := 0; i < 10001; i++ {
+	for i := 0 /*INIT statement*/; i < 10001 /*CONDITION Expression*/; i++ /*POST Statement*/ {
 		fmt.Println(i)
 	}
 }
+//the INIT statement: executed before the first iteration
+//the CONDITION expression: evaluated before every iteration
+//the POST statement: executed at the end of every iteration
