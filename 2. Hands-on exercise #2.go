f1bbf411e4d95c6121cf0cfbed0a3b762886de7c
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-03 20:18:40 +0530

    Add files via upload

---
+++ 2. Hands-on exercise #2.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first  string
+	last   string
+	favice []string
+}
+
+func main() {
+	p1 := person{
+		first:  "James",
+		last:   "Bond",
+		favice: []string{"Mint", "Strawberry", "Chocolate"},
+	}
+	p2 := person{
+		first:  "Todd",
+		last:   "Mcleod",
+		favice: []string{"Martini", "Bubblegum", "Vanilla"},
+	}
+	m := map[string]person{
+		p1.last: p1,
+		p2.last: p2,
+	}
+	for _, v := range m {
+		fmt.Println(v.first, v.last, "Favourite icecreams are")
+		for i, j := range v.favice {
+			fmt.Println("\t", i, j)
+
+		}
+		fmt.Println("______________________")
+	}
+}
