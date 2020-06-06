e014018c3821e0c35f92c0eaa500b97b431e5f36
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-06 22:37:56 +0530

    Update 3.3. While Loop(using for) .go

--- 3.3. While Loop(using for) .go
+++ 3.3. While Loop(using for) .go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	age := 22
+	age := 22	//For loop in golang can be used with or without any conditions.
 	for age > 0 { //There is no while loop in golang.
 		fmt.Println(2021 - age)
 		age--
