6e1e6de1d070cffc66b4dce3aa501c88c1260f38
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-07 23:02:02 +0530

    Update 3.5. Modulus and Remainder.go

--- 3.5. Modulus and Remainder.go
+++ 3.5. Modulus and Remainder.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	for i := 10; i <= 100; i++ {
-		fmt.Printf("Dividing %v with 4 gives remainder %v\n", i, i%4)
+		fmt.Printf("Dividing %v with 4 gives remainder %v\n", i, i%4)	//"%", is the modulus operator. It gives the remainder
+										// when i is divided by 4.
 	}
 }
