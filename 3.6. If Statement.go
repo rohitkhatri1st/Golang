636b6696c2da5af1c718332c7111dc9a9a7bbbb3
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-11 17:12:46 +0530

    Update 3.6. If Statement.go

--- 3.6. If Statement.go
+++ 3.6. If Statement.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	if 42 < 54 {
+	if 42 < 54 {//syntax of if is (if  var declaration;  condition). The expression is optional.
 		fmt.Println("42 is less")
-	} else {
+	} else {//else needs to be written right after end parentheses of if.
 		fmt.Println("54 is less")
 	}
 }
