d03b5d85fa81eaf7a977270143be7bcf325639ac
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-12 22:35:08 +0530

    Update 3.7. Else if and else statement.go

--- 3.7. Else if and else statement.go
+++ 3.7. Else if and else statement.go
@@ -8,8 +8,8 @@ func main() {
 		fmt.Println("42 is less")	
 	} else if x == "James" {//Works if it is just after the parentheses
 		fmt.Println("I am James")
-
-	} else {
+	//else if or else will not run if 'if' statement has executed. Only one of 'if','else if' or 'else' will execute.
+	} else {//Works if it is just after the parentheses
 		fmt.Println("54 is there.")
 	}
 }
