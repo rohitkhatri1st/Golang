ca3e67122bdd67b2be1438dd768248efc076ae2d
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-17 22:27:11 +0530

    Update 4.4. Append.go

--- 4.4. Append.go
+++ 4.4. Append.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 	fmt.Println(x)
-	x = append(x, 52)
+	x = append(x, 52)//appending 52 to x
 	fmt.Println(x)
 	x = append(x, 53, 54, 55)
 	fmt.Println(x)
