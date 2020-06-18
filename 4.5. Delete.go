d6f1264eed6cc2b7b165568b11c912f8c2ec7e76
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-18 21:42:01 +0530

    Update 4.5. Delete.go

--- 4.5. Delete.go
+++ 4.5. Delete.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-	x = append(x[:3], x[6:]...)
-	fmt.Println(x)
+	x = append(x[:3], x[6:]...)//This deletes elements from index 3 to index 5.
+	fmt.Println(x)//Ultimately we are appending x's elements from start to index number 2 and then from index number 6 to end.
 }
