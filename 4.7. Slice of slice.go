7349854a26e6ffdb5989ae153a16bbbcf9fcc6d7
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-21 23:22:19 +0530

    Update 4.7. Slice of slice.go

--- 4.7. Slice of slice.go
+++ 4.7. Slice of slice.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	x := []string{"James", "Bond", "Shaken, not stirred"}
 	y := []string{"Miss", "Moneypenny", "Hellooooo, James"}
-	xy := [][]string{x, y}
+	xy := [][]string{x, y}//defining a slice of slice.
 	fmt.Println(xy)
 	for _, v := range xy {
 		for j, k := range v {
