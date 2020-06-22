e69c536ce8cfb52917ab6d9f127d9aed5e0482d2
rohitkhatri1st <53299308+rohitkhatri1st@users.noreply.github.com>
2020-06-22 23:54:32 +0530

    Update 4.8. Maps.go

--- 4.8. Maps.go
+++ 4.8. Maps.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	m := map[string][]string{
-		"bond_james":      []string{"Shaken, not stirred", "Martinis", "Women"},
+		"bond_james"/*This is key*/:      []string{"Shaken, not stirred", "Martinis", "Women"},//This is Value
 		"moneypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
 		"no_dr":           []string{"Being Evil", "Icecream", "Sunsets"},
 	}
